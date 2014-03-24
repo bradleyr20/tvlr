6d93bf95c7915917bd3e6dcbfa633a313ccc95c4
bradleyr20 <brickman@brickmans-mbp.advancemags.com>
2014-03-24 13:59:01 -0400

    Updates to app prototype

--- html-css-proto/app/scripts/cnt-app-scripts.js
+++ html-css-proto/app/scripts/cnt-app-scripts.js
@@ -5,12 +5,36 @@ $( document ).ready(function() {
     // Your code here.
     $("#overlay").hide();
     
+    $("#overlay").children().hide();
+    
     $("#centro-storico.neighborhood").click(function() {
     		$("#overlay").show();
+    		$("#overlay .location.centro-storico").show();
+    });
+    
+    $("#dom.venue").click(function() {
+    		$("#overlay").show();
+    		$("#overlay .location.dom").show();
+    });
+    
+    $("#urbinati.venue").click(function() {
+    		$("#overlay").show();
+    		$("#overlay .location.laura-urbinati").show();
+    });
+    
+    $("#roscioli.venue").click(function() {
+    		$("#overlay").show();
+    		$("#overlay .location.roscioli").show();
+    });
+    
+    $("#dar-filettaro.venue").click(function() {
+    		$("#overlay").show();
+    		$("#overlay .location.dar-filettaro").show();
     });
     
     $("#overlay").click(function() {
     		$(this).hide();
+    		$(this).children().hide();
     });
  
 });
