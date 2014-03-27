f42dea1caade432cdf0b1dfd9fbe7f85778a8b20
bradleyr20 <brickman@brickmans-mbp.advancemags.com>
2014-03-27 12:12:27 -0400

    migration channel mapping scheme

---
+++ javascripts/channel-mapping.js
@@ -0,0 +1,218 @@
+		var channelMap = {
+		  'cntraveler:arts-culture': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Inspiration'
+		  },
+		  'cntraveler:arts-culture/architecture': {
+		    channel: 'Style & Design',
+		    subchannel: 'Architecture'
+		  },
+		  'cntraveler:arts-culture/art': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Art'
+		  },
+		  'cntraveler:arts-culture/art-galleries': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Museums & Galleries'
+		  },
+		  'cntraveler:arts-culture/books': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Books'
+		  },
+		  'cntraveler:arts-culture/dance': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Theatre & Dance'
+		  },
+		  'cntraveler:arts-culture/design': {
+		    channel: 'Style & Design',
+		    subchannel: 'Design'
+		  },
+		  'cntraveler:arts-culture/movies-film': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Film & TV'
+		  },
+		  'cntraveler:arts-culture/museums': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Museums & Galleries'
+		  },
+		  'cntraveler:arts-culture/music': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Music'
+		  },
+		  'cntraveler:arts-culture/performing-arts': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Theatre & Dance'
+		  },
+		  'cntraveler:cruises': {
+		    channel: 'Travel Tips',
+		    subchannel: 'Cruises'
+		  },
+		  'cntraveler:cruises/7 Perfect Days': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Cruises'
+		  },
+		  'cntraveler:cruises/cruise-itineraries': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Cruises'
+		  },
+		  'cntraveler:cruises/cruise-ships': {
+		    channel: 'Travel Tips',
+		    subchannel: 'Cruises'
+		  },
+		  'cntraveler:cruises/ports': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Cruises'
+		  },
+		  'cntraveler:destinations': {
+		    channel: 'Destinations',
+		    subchannel: 'Tips',
+		  },
+		  'cntraveler:destinations/beaches': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Beach Vacations'
+		  },
+		  'cntraveler:destinations/cities': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Cities'
+		  },
+		  'cntraveler:destinations/islands': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Beach Vacations'
+		  },
+		  'cntraveler:destinations/spas': {
+		    channel: 'Travel Ideas',
+		    subchannel:  'Spas'
+		  },
+		  'cntraveler:destinations/surfing': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Adventure & Outdoors'
+		  },
+		  'cntraveler:family/family-travel': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Family Travel'
+		  },
+		  'cntraveler:features/Concierge/Interests and Ideas': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Inspiration'
+		  },
+		  'cntraveler:features/Concierge/Interests and Ideas/10 Things Not to Do': {
+		    channel: 'Destinations',
+		    subchannel: 'Tips'
+		  },
+		  'cntraveler:features/brooklyn-authors': {
+		    channel: 'Arts & Culture',
+		    subchannel: 'Books'
+		  },
+		  'cntraveler:features/celebrity-travel': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Celebrity Travel'
+		  },
+		  'cntraveler:features/ecotourism': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Eco & Responsible Travel'
+		  },
+		  'cntraveler:features/ecotourism/visionaries-2012': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Eco & Responsible Travel'
+		  },
+		  'cntraveler:features/my-five': {
+		    channel: 'Destinations',
+		    subchannel: 'Tips'
+		  },
+		  'cntraveler:features/wendy-perrin': {
+		    channel: 'Travel Tips',
+		    subchannel: 'Tips'
+		  },
+		  'cntraveler:features/world-savers': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Eco & Responsible Travel'
+		  },
+		  'cntraveler:food-drink': {
+		    channel: 'Food & Drink',
+		    subchannel: ' Restaurants & Cafés'
+		  },
+		  'cntraveler:food-drink/bars-clubs': {
+		    channel: 'Food & Drink',
+		    subchannel: 'Bars & Nightlife'
+		  },
+		  'cntraveler:food-drink/chefs': {
+		    channel: 'Food & Drink',
+		    subchannel: 'Restaurants & Cafés'
+		  },
+		  'cntraveler:food-drink/food': {
+		    channel: 'Food & Drink',
+		    subchannel: 'Restaurants & Cafés'
+		  },
+		  'cntraveler:food-drink/hotel-restaurants': {
+		    channel: 'Food & Drink',
+		    subchannel: 'Restaurants & Cafés'
+		  },
+		  'cntraveler:food-drink/restaurants': {
+		    channel: 'Food & Drink',
+		    subchannel: 'Restaurants & Cafés'
+		  },
+		  'cntraveler:hotels': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Hotels'
+		  },
+		  'cntraveler:hotels/Hotel Slideshows Pools Spas': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Hotels'
+		  },
+		  'cntraveler:hotels/Hotel Slideshows Themes': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Hotels'
+		  },
+		  'cntraveler:hotels/historic-hotels': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Hotels'
+		  },
+		  'cntraveler:shopping-style': {
+		    channel: 'Style & Design',
+		    subchannel: 'Shopping'
+		  },
+		  'cntraveler:shopping-style/fashion': {
+		    channel: 'Style & Design',
+		    subchannel: 'Fashion'
+		  },
+		  'cntraveler:shopping-style/markets': {
+		    channel: 'Style & Design',
+		    subchannel: 'Shopping'
+		  },
+		  'cntraveler:shopping-style/shopping': {
+		    channel: 'Style & Design',
+		    subchannel: 'Shopping'
+		  },
+		  'cntraveler:tips': {
+		    channel: 'Travel Tips',
+		    subchannel: 'Tips'
+		  },
+		  'cntraveler:travel-agent-finder/united-states': {
+		    channel: 'Travel Tips',
+		    subchannel: 'Travel Planners'
+		  },
+		  'cntraveler:trip-planning': {
+		    channel: 'Destinations',
+		    subchannel: 'Tips'
+		  },
+		  'cntraveler:trip-planning/travel-guides': {
+		    channel: 'Destinations',
+		    subchannel: 'Tips'
+		  },
+		  'cntraveler:trip-planning/travel-tips': {
+		    channel: 'Destinations',
+		    subchannel: 'Tips'
+		  },
+		  'cntraveler:vacations/all-inclusive': {
+		    channel: 'Travel Tips',
+		    subchannel: 'Specialty Travel'
+		  },
+		  'cntraveler:vacations/vacation-spots': {
+		    channel: 'Travel Ideas',
+		    subchannel: 'Inspiration'
+		  },
+		  'cntraveler:wendy-perrin/Perrin Report': {
+		    channel: 'Travel Tips',
+		    subchannel: 'Tips'
+		  },
+		};
